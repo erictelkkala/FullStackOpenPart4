658e728b5a0917b94e26b21926b45d6271bd3244
Eric Telkkälä <52538447+erictelkkala@users.noreply.github.com>
2022-07-04 18:52:27 +0300

    Refactored the logger, schema, router and config to separate files

--- index.js
+++ index.js
@@ -1,40 +1,27 @@
-require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGO_URI
-mongoose.connect(mongoUrl)
-
-app.use(cors())
 app.use(express.json())
+app.use(cors())
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogRouter = require('./controllers/blogRouter')
+app.use('/api/blogs', blogRouter)
 
-app.get('/api/blogs', (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs)
+const mongoUrl = config.MONGO_URI
+mongoose
+    .connect(mongoUrl)
+    .then(() => {
+        logger.info('Connected to MongoDB')
     })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog.save().then((result) => {
-        response.status(201).json(result)
+    .catch((error) => {
+        logger.error('Error connecting to MongoDB:', error.message)
     })
-})
 
-const PORT = 3003
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+const server = http.createServer(app)
+const PORT = config.PORT
+server.listen(PORT, () => {
+    logger.info(`Server running on port ${PORT}`)
 })
