d089e42ef1fc74786c3d7ce5bc3c4f8f87adee5c
Eric Telkkälä <52538447+erictelkkala@users.noreply.github.com>
2022-07-05 16:31:48 +0300

    Task 4.5 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,16 +2,32 @@ const dummy = (blogs) => {
     console.log('dummy blogs:', blogs)
     return 1
 }
-  
+
 const totalLikes = (blogs) => {
     var total = 0
-    blogs.forEach(blog => {
+    blogs.forEach((blog) => {
         total += blog.likes
-    }
-    )
+    })
     return total
 }
 
+const favoriteBlog = (blogs) => {
+    var max = 0
+    var maxBlog = blogs[0]
+    blogs.forEach((blog) => {
+        if (blog.likes > max) {
+            max = blog.likes
+            maxBlog = blog
+        }
+    })
+    console.log('favoriteBlog:', maxBlog)
+    // Remove unnecessary properties from the object
+    delete maxBlog.__v && delete maxBlog._id && delete maxBlog.url
+    return maxBlog
+}
+
 module.exports = {
-    dummy, totalLikes
+    dummy,
+    totalLikes,
+    favoriteBlog,
 }
