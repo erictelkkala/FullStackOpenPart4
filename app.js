2982303e0ef5dedc2c4a89258a4d50a68e6dbe67
Eric Telkkälä <52538447+erictelkkala@users.noreply.github.com>
2022-07-06 15:59:15 +0300

    Refactor files to the correct structure

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const blogRouter = require('./controllers/blogRouter')
+
+const mongodbURI = config.MONGODB_URI
+mongoose
+    .connect(mongodbURI)
+    .then(() => {
+        logger.info('Connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('Error connecting to MongoDB:', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,24 +1,7 @@
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-app.use(express.json())
-app.use(cors())
-const mongoose = require('mongoose')
+const app = require('../app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const blogRouter = require('./controllers/blogRouter')
-app.use('/api/blogs', blogRouter)
-
-const mongoUrl = config.MONGO_URI
-mongoose
-    .connect(mongoUrl)
-    .then(() => {
-        logger.info('Connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('Error connecting to MongoDB:', error.message)
-    })
 
 const server = http.createServer(app)
 const PORT = config.PORT
